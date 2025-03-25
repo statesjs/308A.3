d223d4f0d39df8697b7a07aa8a94a1ef56bc5a2a
statesjs <statesjs@gmail.com>
2025-03-25 12:06:40 -0700

    finished making an object for part1, need to implment range errors

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 import { central, db1, db2, db3, vault } from "./databases.js";
 
 //Part 1: The Scenario
+//async implicitly wraps into a Promise.resolve
 async function getUserData(id) {
   const dbs = {
     db1: db1,
@@ -10,10 +11,22 @@ async function getUserData(id) {
   };
   const returnedValue = await central(id);
   const basicInfo = await dbs[returnedValue](id);
-  console.log(basicInfo);
-  console.log(returnedValue);
+  const returnedValueVault = await vault(id);
+
+  return {
+    id,
+    name: returnedValueVault.name,
+    username: basicInfo.username,
+    email: returnedValueVault.email,
+    address: returnedValueVault.address,
+    phone: returnedValueVault.phone,
+    website: basicInfo.website,
+    company: basicInfo.company,
+  };
 }
-getUserData(2);
+getUserData(2).then((user2) => {
+  console.log(user2);
+});
 
 //dbs contain the username,website, and company
-//the vault will contain the name,
+//the vault will contain the name, email, address, and phone data
