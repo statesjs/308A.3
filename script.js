9fb8162c0636c8131a6bd8932a67450424676590
statesjs <statesjs@gmail.com>
2025-03-25 12:55:07 -0700

    need to implment errors for specific databases

--- script.js
+++ script.js
@@ -4,29 +4,43 @@ import { central, db1, db2, db3, vault } from "./databases.js";
 //Part 1: The Scenario
 //async implicitly wraps into a Promise.resolve
 async function getUserData(id) {
+  if (typeof id !== "number" || id < 1 || id > 10) {
+    throw new Error("Invalid ID: must be a number between 1 and 10.");
+  }
+
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3,
   };
-  const returnedValue = await central(id);
-  const basicInfo = await dbs[returnedValue](id);
-  const returnedValueVault = await vault(id);
 
-  return {
-    id,
-    name: returnedValueVault.name,
-    username: basicInfo.username,
-    email: returnedValueVault.email,
-    address: returnedValueVault.address,
-    phone: returnedValueVault.phone,
-    website: basicInfo.website,
-    company: basicInfo.company,
-  };
+  try {
+    const returnedValue = await central(id);
+    const [basicInfo, returnedValueVault] = await Promise.all([
+      dbs[returnedValue](id),
+      vault(id),
+    ]);
+
+    return {
+      id,
+      name: returnedValueVault.name,
+      username: basicInfo.username,
+      email: returnedValueVault.email,
+      address: returnedValueVault.address,
+      phone: returnedValueVault.phone,
+      website: basicInfo.website,
+      company: basicInfo.company,
+    };
+  } catch (error) {
+    console.log(error.message);
+  }
+  return Promise.reject(err);
 }
-getUserData(2).then((user2) => {
-  console.log(user2);
-});
+getUserData(11)
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => console.log(`theres was an error: ${err}`));
 
 //dbs contain the username,website, and company
 //the vault will contain the name, email, address, and phone data
